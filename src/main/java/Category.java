67b0c1ca9d9867415871f45fb51910d3fbab5f5c
Topaxx <magowms@gmail.com>
2019-11-24 17:02:27 +0000

    Criacao das classes da Category

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,2 +1,32 @@
 public class Category {
+
+    private String nome;
+    private String description;
+    private String color;
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
